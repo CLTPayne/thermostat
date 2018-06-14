437fa2c957a28cd17bfb86e377a67559e75e8591
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-14 23:59:57 +0100

    Feature to increase temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,7 +9,12 @@ describe('Thermostat', function() {
   });
 
   it('Thermostat starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('Can be increased in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  })
 
 });
